5027befd641936c984c6005c5226a6ea3f69fa98
ChefEl33 <icheftech@gmail.com>
2026-01-08 11:38:53 -0600

    Create User.js

---
+++ backend/src/models/User.js
@@ -0,0 +1,23 @@
+// User model schema
+const { Sequelize, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  
+  return User;
+};
